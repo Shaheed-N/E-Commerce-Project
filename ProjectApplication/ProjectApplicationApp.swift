fceea5ca7b12ff61bc6205818fa5efa5f35092ff
Shaheed-N <username@example.com>
2022-12-11 16:30:59 +0400

    Initial Commit

---
+++ ProjectApplication/ProjectApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApplicationApp.swift
+//  ProjectApplication
+//
+//  Created by Shahid on 11.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
